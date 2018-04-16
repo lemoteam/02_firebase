9a73fac73aaebf55e2afe0bff5c7731b3cf1bd87
PINOT Arnaud <apinot@g-a106-12.local>
2018-04-16 17:30:47 +0200

    first commit

---
+++ Assets/Lemo/Scripts/AuthManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+
+public class AuthManager : MonoBehaviour {
+
+	// Firebase API Variables
+	Firebase.Auth.FirebaseAuth auth;
+
+	void Awake() {
+		auth = FirebaseAuth.DefaultInstance;
+	}
+
+	public void SignUpNewUser(string email, string password) {
+		
+		auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+			if (task.IsCanceled) {
+				Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+				return;
+			}
+			if (task.IsFaulted) {
+				Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+				return;
+			}
+
+			// Firebase user has been created.
+			Firebase.Auth.FirebaseUser newUser = task.Result;
+			Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+				newUser.DisplayName, newUser.UserId);
+		});
+
+	}
+}
---
+++ Assets/Lemo/Scripts/DatabaseManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatabaseManager : MonoBehaviour {
+
+	public static DatabaseManager sharedInstance = null;
+
+	/// <summary>
+	/// Awake this instance and initialize sharedInstance for Singleton pattern
+	/// </summary>
+	void Awake() {
+		if (sharedInstance == null) {
+			sharedInstance = this;
+		} else if (sharedInstance != this) {
+			Destroy (gameObject);  
+		}
+
+		DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
