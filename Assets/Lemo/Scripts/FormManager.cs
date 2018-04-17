2981e8f8f2c4984cd419fe08f4dddcb113a60527
Pinot Arnaud <pinot.arnaud@gmail.com>
2018-04-17 10:26:46 +0200

    Fix firebase bug with latest SDK 4.5.1

--- Assets/Lemo/Scripts/FormManager.cs
+++ Assets/Lemo/Scripts/FormManager.cs
@@ -32,7 +32,7 @@ public class FormManager : MonoBehaviour {
 	/// <summary>
 	/// Validates the email input
 	/// </summary>
-	public void ValidateEmail() {
+	public void ValidateEmail() { 
 		string email = emailInput.text;
 		var regexPattern = @"^(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
                 + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
